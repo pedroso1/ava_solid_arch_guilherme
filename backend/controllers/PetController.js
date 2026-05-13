ed2256356160f4c100eda762a4fbfa43cda80230
pedroso1 <guiwpedroso@gmail.com>
2026-05-12 22:42:33 -0300

    Task 5: Busca e Remoção de Pet por ID (getPetById e removePetById)

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -74,11 +74,45 @@ module.exports = class PetController {
   }
 
   static async getPetById(req, res) {
-  
+    const id = req.params.id;
+
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(422).json({ message: 'ID inválido!' });
+        return;
+      }
+
+      const pet = await Pet.findOne({ _id: id });
+
+      if (!pet) {
+        res.status(404).json({ message: 'Pet não encontrado!' });
+        return;
+      }
+
+      res.status(200).json({
+        pet: pet,
+      });
   }
 
   static async removePetById(req, res) {
-    
+    const id = req.params.id;
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+    const pet = await Pet.findOne({ _id: id });
+
+    if (!pet) {
+      res.status(404).json({ message: 'Pet não encontrado!' });
+      return;
+    }
+
+    if (pet.user._id.toString() !== user._id.toString()) {
+      res.status(422).json({
+        message: 'Houve um problema ao acessar a sua solicitação!',
+      });
+      return;
+    }
+
+    await Pet.findByIdAndDelete(id);
+    res.status(200).json({ message: 'Pet removido com sucesso!' }); 
   }
  
 
