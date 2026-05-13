60363023a82b0db9f459192b59c84baf29344776
pedroso1 <guiwpedroso@gmail.com>
2026-05-12 22:38:44 -0300

    Task 4: Listagem de Pets e Adoções do Usuário (getAllUserPets e getAllUserAdoptions)

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -56,11 +56,21 @@ module.exports = class PetController {
   }
 
   static async getAllUserPets(req, res) {
-
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+    const pets = await Pet.find({ 'user._id': user._id }).sort('-createdAt');
+    res.status(200).json({
+      pets,
+    });
   }
 
   static async getAllUserAdoptions(req, res) {
- 
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+    const pets = await Pet.find({ 'adopter._id': user._id }).sort('-createdAt');
+    res.status(200).json({
+      pets,
+    });
   }
 
   static async getPetById(req, res) {
