16c15b8a051bcd4c55685ad0490afe2cf8af564e
pedroso1 <guiwpedroso@gmail.com>
2026-05-12 22:30:56 -0300

    Task 2: Criação do Pet com Upload Múltiplo (create)

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -5,6 +5,46 @@ const getUserByToken = require('../helpers/get-user-by-token');
 
 module.exports = class PetController {
   static async createPet(req, res) {
+    const { name, age, weight, color } = req.body;
+    const images = req.files;
+    const available = true;
+
+    if (!name || !age || !weight || !color) {
+      res.status(422).json({ message: 'Todos os campos são obrigatórios!' });
+      return;
+    }
+
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    const pet = new Pet({
+      name,
+      age,
+      weight,
+      color,
+      available,
+      images: [],
+      user: {
+        _id: user._id,
+        name: user.name,
+        image: user.image,
+        phone: user.phone,
+      },
+    });
+
+    images.map((image) => {
+      pet.images.push(image.filename);
+    });
+
+    try {
+      const newPet = await pet.save();
+      res.status(201).json({
+        message: 'Pet cadastrado com sucesso!',
+        newPet,
+      });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
 
   }
 
