b30190f016f4fd297a0b528bcc638e735dadfb70
pedroso1 <guiwpedroso@gmail.com>
2026-05-12 22:34:01 -0300

    Task 3: Função de Resgatar todos os Pets (getAll)

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -49,7 +49,10 @@ module.exports = class PetController {
   }
 
   static async getAll(req, res) {
-
+    const pets = await Pet.find().sort('-createdAt');
+    res.status(200).json({
+      pets: pets,
+    });
   }
 
   static async getAllUserPets(req, res) {
